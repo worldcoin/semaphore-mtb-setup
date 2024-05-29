84ef2275ecc46078f0cbd6183485a086a62776f4
Eric Woolsey <ewoolsey@ualberta.ca>
2024-05-29 15:46:12 -0700

    WIP tests

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ toolchain go1.22.1
 require (
 	github.com/consensys/gnark v0.10.0
 	github.com/consensys/gnark-crypto v0.12.2-0.20240504013751-564b6f724c3b
+	github.com/stretchr/testify v1.8.4
 	github.com/urfave/cli/v2 v2.25.7
 	github.com/worldcoin/ptau-deserializer v0.2.0
 )
@@ -16,11 +17,13 @@ require (
 	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/consensys/bavard v0.1.13 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fxamacker/cbor/v2 v2.5.0 // indirect
 	github.com/google/pprof v0.0.0-20230817174616-7a8ec2ada47b // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/ronanh/intcomp v1.1.0 // indirect
 	github.com/rs/zerolog v1.30.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
@@ -29,6 +32,7 @@ require (
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	rsc.io/tmplfunc v0.0.3 // indirect
 )
 
---
+++ main_test.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+
+	// "crypto/rand"
+	// "math/big"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"testing"
+
+	// "github.com/consensys/gnark-crypto/ecc/bn254/fr"
+	// "github.com/consensys/gnark-crypto/ecc/bn254/fr/pedersen"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr/pedersen"
+	"github.com/consensys/gnark/backend/groth16/bn254/mpcsetup"
+	ecdsa "github.com/worldcoin/semaphore-mtb-setup/examples/ecdsa"
+
+	deserializer "github.com/worldcoin/ptau-deserializer/deserialize"
+)
+
+func TestWriteTo(t *testing.T) {
+	err := testSetup()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func testSetup() error {
+	ptauFilePath := "examples/ecdsa/ppot_0080_13.ptau"
+	ptauDownloadLink := "https://pse-trusted-setup-ppot.s3.eu-central-1.amazonaws.com/pot28_0080/ppot_0080_13.ptau"
+	err := ensureFileExists(ptauFilePath, ptauDownloadLink)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Building R1CS...")
+	r1cs, err := ecdsa.BuildR1CS()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Reading ptau file...")
+	ptau, err := deserializer.ReadPtau(ptauFilePath)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Converting ptau to phase1...")
+	phase1, err := deserializer.ConvertPtauToPhase1(ptau)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Initializing phase2...")
+	phase2, evals := mpcsetup.InitPhase2(r1cs, &phase1)
+
+	fmt.Println("Initializing pedersen keys...")
+	pedersenKeys := PedersenKeys{}
+	pedersenKeys.PK, pedersenKeys.VK, err = pedersen.Setup(evals.G1.VKK)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Running phase2 contribution...")
+	phase2Final := phase2
+	pedersenKeysFinal := pedersenKeys
+	phase2Final.Contribute()
+	pedersenKeysFinal.Contribute()
+
+	fmt.Println("Running phase2 verification...")
+	mpcsetup.VerifyPhase2(&phase2, &phase2Final)
+
+	return nil
+}
+
+func ensureFileExists(filePath, url string) error {
+	// Check if the file exists
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		// File does not exist, download it
+		fmt.Printf("File does not exist, downloading from %s...\n", url)
+		if err = downloadFile(filePath, url); err != nil {
+			return fmt.Errorf("failed to download file: %v", err)
+		}
+	} else if err != nil {
+		return fmt.Errorf("failed to check if file exists: %v", err)
+	}
+
+	// Open the file
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %v", err)
+	}
+	defer file.Close()
+
+	// You can use the file here (e.g., read from it, etc.)
+	fmt.Println("File opened successfully")
+	return nil
+}
+
+func downloadFile(filePath, url string) error {
+	// Create the directories if they do not exist
+	if err := os.MkdirAll(filepath.Dir(filePath), 0755); err != nil {
+		return fmt.Errorf("failed to create directories: %v", err)
+	}
+
+	// Create the file
+	out, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %v", err)
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("failed to download file: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check server response
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to write to file: %v", err)
+	}
+
+	return nil
+}
